b0d9d7ddf45c95b4594fa4d45f4e2d512d268f6a
adrian.gralak95 <adrian.gralak95@gmail.com>
2016-10-29 13:09:40 -0400

    change

---
+++ concept.py
@@ -0,0 +1,24 @@
+class concept:
+  def __init__(self):
+    self.keywords = {}
+    self.topics = {}
+    self.sentiment = {sentiment : "none", score : 0}
+  def importFromJSON(JSON):
+    """
+    takes a JSON file as a string and fills out all the concept attributes#
+    and topics as expected.
+    """
+    #TODO finish me
+    pass
+  def compare (self, other):
+    """
+    compares two concepts and returns a relevance score between 0 and 1
+    """
+    pass
+
+  def top10chatrooms(self, chatroomList):
+    """
+    takes the chatrooms list and returns top 10 most relevant chatrooms
+    """
+    pass
+    
